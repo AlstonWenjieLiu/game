2ae85df30fa74c3ac4b3c33d5af557ea8b0d1cbe
ALSTONLIU <alstonl17@nycstudent.net>
2026-01-12 12:21:43 -0500

    Pass scanner across scanners.

---
+++ src/Logic.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Logic {
+    private Scanner scan;
+
+    public Logic(Scanner scan) {
+        this.scan = scan;
+    }
+
+    public void start() {
+
+        System.out.println("Would you like to create a new account? (Enter 1) (Otherwise enter 2)");
+    }
+}
