e30b25bd4caa35e9d233aec414d789baa23157d2
ALSTONLIU <alstonl17@nycstudents.net>
2026-01-27 10:41:38 -0500

    Proper documentation.

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,25 +1,49 @@
 import java.util.Scanner;
 
+/**
+ * This class represents enemies
+ * 
+ * @author ALSTONLIU 
+ */
 public class Enemy {
     //Dependencies
+    /** The scanner */
     private Scanner scan;
 
     //instance variables
+    /** The level of the enemy */
     private int level;
+
+    /** The health of the enemy */
     private double health;
+
+    /** The strength of the enemy */
     private double strength;
 
     //Constructors
+    /**
+     * Instantiates the Enemy object.
+     *
+     * @param scan The passed scanner
+     */
     public Enemy(Scanner scan) {
         this.scan = scan;
     }
 
+    /**
+     * Instantiates the Enemy object.
+     *
+     * @param level The enemy's level
+     */
     public Enemy(int level) {
         this.level = level;
         health = level;
         strength = level;
     }
 
+    /**
+     * Instantiates the Enemy object.
+     */
     public Enemy() {
         this.level = 1; //Default stats
         health = level;
@@ -27,11 +51,17 @@ public class Enemy {
     }
 
     //Methods
+    /**
+     * Prints out the enemy's announcements
+     */
     public void announce() {
         System.out.println("GRR!");
         System.out.println("I'm level " + getLevel() + ", you'll never defeat me!");
     }
 
+    /**
+     * Prints out the enemy's information
+     */
     public void getStats() {
         System.out.println(" - - - Enemy Stats - - - ");
         System.out.println("Level:" + getLevel());
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 import java.util.Scanner;
 
+/**
+ * This class is the runner class so it does not have any constructors
+ *
+ * @author ALSTONLIU
+ */
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in); //This scanner is passed to other classes.
+        /** This scanner is passed to other classes */
+        Scanner scan = new Scanner(System.in);
         Player playerScanner = new Player(scan, 0, "stub");
         Enemy enemyScanner = new Enemy(scan);
         Logic LogicScanner = new Logic(scan);
