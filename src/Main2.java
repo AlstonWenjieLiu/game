af9f01cbffafbb5b65617cf2a1677c932b2919cd
ALSTONLIU <alstonl17@nycstudents.net>
2026-01-27 10:51:12 -0500

    finish rectangl

---
+++ src/Main2.java
@@ -0,0 +1,8 @@
+public class Main2 {
+    public static void main(String[] args) {
+        Rectangle rect1 = new Rectangle(5, 6);
+        boolean square = rect1.isSquare();
+        System.out.println(square);
+
+    }
+}
